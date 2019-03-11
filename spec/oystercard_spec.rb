025314b4fbb8f692401f5f8abfd470486c95d3d6
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-11 15:29:51 +0000

    Top-up test completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,15 @@
 require 'oystercard'
 
-describe Oystercard do
+  describe Oystercard do
     it 'has a balance of 0' do
-    expect(subject.balance).to eq(0)
+      expect(subject.balance).to eq(0)
+    end
+
+  describe '#top_up' do
+    it "tops up by £5" do
+      oystercard = Oystercard.new
+      oystercard.top_up(5)
+      expect(subject.balance). to eq(5)
+    end
   end
 end
