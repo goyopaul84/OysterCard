e2cc0cd768641b468c482e3a77d315e6c0d55c24
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-12 16:41:29 +0000

    New station class created and attributes exposed

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+describe Station do
+  let(:name) { double :name }
+  let(:zone) { double :zone }
+  it "exposes the name" do
+    station = Station.new(name, zone)
+    p station.name
+    expect(station.name).to eq name
+  end
+
+  it "exposes the zone" do
+    station = Station.new(name, zone)
+    expect(station.zone).to eq zone
+  end
+end
